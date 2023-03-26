82ad83ff6f689cc770072cc7820896f8703f8a08
Andrey B <fbandrey@gmail.com>
2023-03-27 02:09:18 +0700

    updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 import { SEO } from 'next-seo.config';
@@ -7,16 +8,32 @@ import './styles.css';
 
 const KedrovyDvorApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <MantineProvider
-      withCSSVariables
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{ colorScheme: 'light' }}
-    >
-      <Notifications position='top-center' />
-      <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
-    </MantineProvider>
+    <>
+      {/* Google Tag Manager - Global base code */}
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'GTM-WQPHDDP');
+          `,
+        }}
+      />
+      <MantineProvider
+        withCSSVariables
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme: 'light' }}
+      >
+        <Notifications position='top-center' />
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
   )
 };
 
