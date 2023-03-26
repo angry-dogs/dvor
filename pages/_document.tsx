bbdbca339914f976b059d4d4c062c8795f498a30
Andrey B <fbandrey@gmail.com>
2023-03-27 02:51:54 +0700

    updates

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta name="yandex-verification" content="9b8d7609f163326d" />
+      </Head>
       <body>
         <noscript>
           <iframe
